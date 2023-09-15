a776665e407d84b60c5d8ed18e7a84c5c740a976
Monashah1 <ms4502@nyu.edu>
2023-09-15 14:01:51 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -22,9 +22,11 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = int("4")
+        ans = 4
+        answer = int(ans)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = int("2")
+        ans = 2
+        answer = int(ans)
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?

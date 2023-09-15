7e435461fb51d4844482787c303049ecc2526e8e
Monashah1 <ms4502@nyu.edu>
2023-09-15 00:12:04 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -37,10 +37,12 @@ def welcome_assignment_answers(question):
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
     
-    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-    print(welcome_assignment_answers(debug_question))
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+    
+    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    print(welcome_assignment_answers(debug_question))
+    
     
     debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
     print(welcome_assignment_answers(debug_question))
